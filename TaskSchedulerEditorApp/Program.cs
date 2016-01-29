08619506806b6437d8b2b5f306daf154f598307e
kenjiuno <h8mastre@gmail.com>
2016-01-29 16:31:41 +0900

    0.1

---
+++ TaskSchedulerEditorApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Win32.TaskScheduler;
+using System;
+using System.Windows.Forms;
+
+namespace TaskSchedulerEditorApp {
+    class Program {
+        [STAThread]
+        static void Main(string[] args) {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            for (int x = 0, cx = args.Length; x < cx; x++) {
+                if ("/tn".Equals(args[x], StringComparison.InvariantCultureIgnoreCase)) {
+                    x++;
+                    if (x < cx) {
+                        using (TaskService ts = new TaskService()) {
+                            var t = ts.GetTask(args[x]);
+                            if (t.ShowEditor()) {
+                                t.RegisterChanges();
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
